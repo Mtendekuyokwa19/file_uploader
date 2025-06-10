7e0fd2ce72307dac13b2803b47499968dad64bd0
mtende <mtendekuyokwa19@gmail.com>
2025-06-10 18:23:37 +0200

    development of ui for login and signup

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const router = require("./routes/routes");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", router);
+app.use("signup", router);
+app.use("/login", router);
 
 app.listen(3000, function () {
   console.log("listening");
