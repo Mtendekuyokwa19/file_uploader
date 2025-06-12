7f2bdc36040d6b2b06c12d17792616c6d5c3e67a
mtende <mtendekuyokwa19@gmail.com>
2025-06-12 08:50:37 +0200

    fixing auth on passport login

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const path = require("node:path");
-
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+// const { PrismaClient } = require("./generated/prisma/");
+const { PrismaClient } = require("@prisma/client");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const express = require("express");
 const app = express();
 const router = require("./routes/routes");
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.session());
+
+app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", router);
 app.use("signup", router);
 app.use("/login", router);
-
+const random = Math.floor(Math.random() * 50);
 app.listen(3000, function () {
-  console.log("listening");
+  console.log(`listening ${3000 + random}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
+const LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport");
+// const { PrismaClient } = require("../generated/prisma/");
+const { PrismaClient } = require("../generated/prisma/");
+const bcrypt = require("bcryptjs");
+const prisma = new PrismaClient();
+require("dotenv").config();
 
 router.get("/", (req, res) => {
   res.render("index");
@@ -10,4 +17,78 @@ router.get("/login", (req, res) => {
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
+
+router.post("/signup", (req, res) => {
+  const user = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  bcrypt.hash(user.password, salt).then(async (password) => {
+    await prisma.user
+      .create({
+        data: {
+          name: user.fullname,
+          email: user.email,
+          password: password,
+        },
+      })
+      .then(() => {
+        res.redirect("/login");
+      });
+  });
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          name: username,
+        },
+      });
+      console.log(user);
+      if (!user) {
+        return done(null, false, { message: "wrong fullname" });
+      }
+      const password_check = await bcrypt.compare(password, user.password);
+      if (password_check) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "wrong password" });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  console.log(null, user.id);
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    done(null, user);
+  } catch (err) {
+    done(false);
+  }
+});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    res.redirect("/");
+  });
+});
 module.exports = router;
+// main();
+//
